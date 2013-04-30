5efb50b054a3290d1c6b36e0602843c1e9f8016b
Luke Palmer <luqui@clozure.com>
2013-04-30 15:18:55 -0600

    Added a converter from lambda calculus to CCC compositions.

---
+++ ccc.hs
@@ -0,0 +1,31 @@
+-- lambda calculus to composition notation in CCCs
+
+type Name = String
+
+data Term 
+    = Free Name
+    | Var Int
+    | Lambda Term
+    | App Term Term
+    deriving Show
+
+data CCMor
+    = CName Name
+    | Id
+    | Then CCMor CCMor
+    | Curry CCMor          -- [ a*b -> c ]      -> [ a -> (b -> c) ]
+    | Uncurry CCMor        -- [ a -> (b -> c) ] -> [ a*b -> c ]
+    | Unit                 -- a -> 1
+    | Proj1                -- a * b -> a
+    | Proj2                -- a * b -> b
+    | Bracket CCMor CCMor  -- [ a -> b ] -> [ a -> c ] -> [ a -> b*c ]
+    deriving Show
+
+
+convert :: Term -> CCMor
+convert (Free n)   = CName n
+convert (Var i)    = foldr Then Proj2 (replicate i Proj1) 
+convert (Lambda t) = Curry (convert t)    -- (X * a -> b) -> (X -> (a -> b))
+convert (App x y)  = Then (Bracket (convert x) (convert y)) (Uncurry Id)
+
+
