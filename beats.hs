932224ffc428869236542fc716036034f34c70dd
Luke Palmer <lrpalmer@gmail.com>
2016-01-10 21:00:51 -0800

    Well... it's something

--- beats.hs
+++ beats.hs
@@ -1,32 +1,66 @@
 import Control.Applicative
-import Control.Concurrent (threadDelay)
-import Control.Monad (replicateM_)
+import Control.Concurrent (threadDelay, forkIO)
+import Control.Monad (replicateM, replicateM_, forever, join)
+import Data.IORef
 import qualified System.MIDI as MIDI
+import qualified Control.Monad.Random as Rand
 
-note :: Int -> Int
-note 0 = 60
-note 1 = 62
-note 2 = 64
-note 3 = 65
-note 4 = 67
-note 5 = 69
-note 6 = 71
-note 7 = 72
+class GenRandom a where
+    genRandom :: (Rand.RandomGen g) => Int -> Rand.Rand g a
 
-playNote :: MIDI.Connection -> Int -> Double -> IO ()
+
+
+
+newtype Note = N Int
+    deriving (Show)
+instance GenRandom Note where
+    genRandom _ = N <$> Rand.uniform [0..7]
+
+newtype Vel = V Double
+    deriving (Show)
+instance GenRandom Vel where
+    genRandom _ = V <$> Rand.getRandomR (0,1)
+
+data Beat 
+    = Rest
+    | Note Note Vel
+    | Concat [Beat]
+    deriving (Show)
+instance GenRandom Beat where
+    genRandom 0 = join $ Rand.uniform [pure Rest, Note <$> genRandom 0 <*> genRandom 0]
+    genRandom d = join $ Rand.uniform [
+        do s <- Rand.getRandomR (2,4)
+           Concat <$> replicateM s (genRandom (d-1))
+      ]
+
+midiVel :: Vel -> Int
+midiVel (V v) = max 0 . min 127 $ floor (127*v)
+
+midiNote :: Note -> Int
+midiNote (N i)  
+    | i < 8 = [60,62,64,65,67,69,71,72] !! i
+    | otherwise = 0
+
+playNote :: MIDI.Connection -> Note -> Vel -> IO ()
 playNote conn n v = do
-    MIDI.send conn (MIDI.MidiMessage 1 (MIDI.NoteOn (note n) (floor (127 * v))))
-    MIDI.send conn (MIDI.MidiMessage 1 (MIDI.NoteOn (note n) 0))
+    MIDI.send conn (MIDI.MidiMessage 1 (MIDI.NoteOn (midiNote n) (midiVel v)))
+    MIDI.send conn (MIDI.MidiMessage 1 (MIDI.NoteOn (midiNote n) 0))
 
 delay :: Double -> IO ()
 delay sec = threadDelay (floor (10^6 * sec))
 
-main :: IO ()
-main = do
+interp :: MIDI.Connection -> Double -> Beat -> IO ()
+interp conn tempo = go (4*60/tempo)
+    where
+    go time Rest = delay time
+    go time (Note n v) = playNote conn n v >> delay time
+    go time (Concat bs) = mapM_ (go (time / fromIntegral (length bs))) bs
+
+mkPlayer :: IO (Beat -> IO ())
+mkPlayer = do
     conn <- MIDI.openDestination =<< head <$> MIDI.enumerateDestinations
-    replicateM_ 8 $ do
-        playNote conn 0 1
-        delay (1/2)
-        playNote conn 1 1
-        delay (1/2)
-    MIDI.close conn
+    ref <- newIORef Rest
+    _ <- forkIO . forever $ do
+        beat <- readIORef ref
+        interp conn 100 beat
+    return $ writeIORef ref
