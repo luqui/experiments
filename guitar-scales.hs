4baecf5850b4df485562a551959ec943aeebecc1
Luke Palmer <lrpalmer@gmail.com>
2017-12-10 22:01:06 -0800

    A couple practice games implemented

--- guitar-scales.hs
+++ guitar-scales.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE MultiWayIf, TupleSections #-}
 
 import Data.Maybe (catMaybes)
+import Control.Applicative (liftA2)
 import Control.Arrow (second)
+import Control.Monad (forever, forM_)
 import Control.Monad.Trans.State (state, evalState)
+import System.IO (hFlush, stdout)
+import qualified Control.Monad.Random as Rand
 
 data Position
     = Position { pName :: String, pFrets :: [[(Int,Int)]] }
@@ -11,7 +15,7 @@ data Position
 positions :: [Position]
 positions = [
     Position {
-        pName = "Phrygian",
+        pName = "phr",
         pFrets = parseFrets 3
             [ "oo-o"
             , "oo-o"
@@ -21,7 +25,7 @@ positions = [
             , "oo-o" ]
     },
     Position {
-        pName = "Locrian",
+        pName = "loc",
         pFrets = parseFrets 7
             [ "oo-o"
             , "-o-o"
@@ -31,7 +35,7 @@ positions = [
             , "oo-o" ]
     },
     Position {
-        pName = "Dorian",
+        pName = "dor",
         pFrets = parseFrets 2
             [ "-o-oo"
             , "-o-oo"
@@ -41,7 +45,7 @@ positions = [
             , "-o-oo" ]
     },
     Position {
-        pName = "Aeolian",
+        pName = "aeo",
         pFrets = parseFrets 6
             [ "-o-oo"
             , "-oo-o"
@@ -51,7 +55,7 @@ positions = [
             , "-o-oo" ]
     },
     Position {
-        pName = "Mixolydian",
+        pName = "mix",
         pFrets = parseFrets 5
             [ "-o-o-"
             , "-o-oo"
@@ -91,5 +95,39 @@ padRender pre post = map ((dashes pre ++) . (++ dashes post))
     where
     dashes n = replicate (2*n) '-'
 
+patternGame :: [Int] -> IO ()
+patternGame strings = do
+    pos <- Rand.evalRandIO $ Rand.uniform positions
+    let question = do
+            mapM_ putStrLn $ zipWith (++) (map show strings) (padRender 1 1 $ renderPosition Nothing (reverse $ map ((pFrets pos !!) . (6-)) strings))
+            putStr $ "Pattern? "
+            answer <- getLine
+            if answer == pName pos
+                then putStrLn "\ESC[1;32mYep!\ESC[0m"
+                else putStrLn "\ESC[1;31mNope!\ESC[0m" >> question
+    question
 
+randPatternGame :: IO ()
+randPatternGame = do
+    strings <- Rand.evalRandIO $ Rand.uniform [[1,2],[3,4],[5,6],[1,2,3],[4,5,6],[1,2,3,4,5,6]]
+    patternGame strings
+
+
+degreeGame :: [Int] -> IO ()
+degreeGame strings = do
+    (pos,deg) <- Rand.evalRandIO $ liftA2 (,) (Rand.uniform positions) (Rand.uniform [1..7])
+    let question = do
+            mapM_ putStrLn $ zipWith (++) (map show strings) (padRender 1 1 $ renderPosition (Just deg) (reverse $ map ((pFrets pos !!) . (6-)) strings))
+            putStr $ "Degree of major? "
+            hFlush stdout
+            answer <- readLn
+            if answer == deg
+                then putStrLn "\ESC[1;32mYep!\ESC[0m"
+                else putStrLn "\ESC[1;31mNope!\ESC[0m" >> question
+    question
+
+randDegreeGame :: IO ()
+randDegreeGame = do
+    strings <- Rand.evalRandIO $ Rand.uniform [[1,2],[3,4],[5,6],[1,2,3],[4,5,6],[1,2,3,4,5,6]]
+    degreeGame strings
 
